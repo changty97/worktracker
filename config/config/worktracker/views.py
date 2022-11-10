2851e1fe2dd23f0a1cabdc3884aa98e048c7ed5c
Tyler Chang <chang.ty97@gmail.com>
2022-11-09 23:20:00 -0800

    Added templates for Frontend

--- config/config/worktracker/views.py
+++ config/config/worktracker/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# from .models import Members
 
-# Create your views here.
+# def index(request):
+#   mymembers = Members.objects.all().values()
+#   output = ""
+#   for x in mymembers:
+#     output += x["firstname"]
+#   return HttpResponse(output)
+
+def index(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
